b1686756dae18aaf00f3b283c1154b0a388c6739
Carlos Jr <junior_recla@hotmail.com>
2020-10-02 10:31:41 -0300

    Desafio de compilação usando javac

---
+++ Classe.java
@@ -0,0 +1,7 @@
+public class Classe{
+	@Override
+	public String toString(){
+		return "\'1, 2, 3, 4, 5, 6, 7, 8, 9 ou 10\'";
+
+	}
+}
---
+++ Teste.java
@@ -0,0 +1,7 @@
+public class Teste{
+	public static void main(String[] args){
+		//System.out.println("\'1, 2, 3, 4, 5, 6, 7, 8, 9 ou 10\'");
+		Classe classe = new Classe();
+		System.out.println(classe);
+	}
+}
